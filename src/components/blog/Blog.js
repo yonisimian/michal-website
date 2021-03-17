d448881191c076df73e7c06b70be80645f82fc51
Yonisimian <yonisimian@gmail.com>
2021-03-17 23:45:46 +0200

    db foundation

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,51 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Article from './Article'
 import Item from './Item'
 import Pagination from '../others/Pagination'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Data from './demoDB.json'
 
 const Blog = () => {
-    let [article, setArticle] = useState();
-    let data = [
-        {
-            title: "כותרת המאמר",
-            summary: "תת כותרת או תיאור המאמר בשני משפטים",
-            src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true"
-        },
-        { title: "כותרת המאמר השני", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "כותרת המאמר השלישי", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "Fourth כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "Fifth כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "Sixth כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "Seventh כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "Eighth כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "Ninth כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "10 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "11 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "12 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "13 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "14 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "15 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "16 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "17 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "18 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "19 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "20 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "21 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "22 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "23 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "24 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "25 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "26 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "27 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "28 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "29 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "30 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "31 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" },
-        { title: "32 כותרת המאמר", summary: "תת כותרת או תיאור המאמר בשני משפטים", src: "https://docs.google.com/document/d/e/2PACX-1vRozLnYZYaToNoi6lzXTZDA2bGOrGX9bW2VFoOUmCi8t4zLvjUvrmExp6VjV_eW9HfcUUzeBgPt36C8/pub?embedded=true" }
-    ]
-    const items = Array.from(data)
+    let [article, setArticle] = useState()
+    const [items, setItems] = useState(Data.articles) // later we fetch
     const [curPage, setCurPage] = useState(0)
     const [curSection, setCurSection] = useState(0)
     const itemsPerPage = 3
@@ -59,6 +22,12 @@ const Blog = () => {
                             curSection={curSection}
                             setCurSection={setCurSection} />
 
+    /*useEffect(() => {
+        fetch('/getAllArticles')
+        .then(res => res)
+        .then(data => setItems(data))
+    }, [])*/
+
     return (
         <section id="blogSection">
             {
@@ -70,7 +39,7 @@ const Blog = () => {
             <>
             <Row>
                 <Col/>
-                {pagination}
+                <Col>{pagination}</Col>
                 <Col/>
             </Row>
             {items
@@ -84,6 +53,7 @@ const Blog = () => {
                             key={index}
                             title={item.title}
                             summary={item.summary}
+                            createdAt={item.createdAt}
                             setArticle={() => setArticle(item.src)}/>
                     </Col>
                 </Row>
@@ -92,7 +62,7 @@ const Blog = () => {
             }
             <Row>
                 <Col/>
-                {pagination}
+                <Col>{pagination}</Col>
                 <Col/>
             </Row>
             </>
--- src/components/blog/Item.js
+++ src/components/blog/Item.js
@@ -1,16 +1,24 @@
+import { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import './item.css'
 
 const Item = (props) => {
+    const [classes, setClasses] = useState('')
+
+    const switchClasses = () => {
+      setClasses(classes === '' ? 'box bounce-7' : '')
+    }
+    
     return (
-        <Card style={{backgroundColor: props.color}}>
+        <Card className={classes} style={{backgroundColor: props.color}}>
             <Card.Img variant="top" src="" />
-            <Card.Body>
+            <Card.Body onDoubleClick={switchClasses}>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>{props.summary}</Card.Text>
                 <Button variant="primary" onClick={props.setArticle}>לקריאת המאמר -&gt;</Button>
             </Card.Body>
+            <Card.Text className="createdAt">{props.createdAt}</Card.Text>
         </Card>
     )
 }
